b929caab30d273cf2f797683dbec1c809b2b62e9
ElmerAS <eialfaro29@gmail.com>
2018-06-08 15:33:47 -0500

    Iniciando proyecto

---
+++ src/main/java/pe/edu/sistemas/sisasistlab/SisasistlabApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.sistemas.sisasistlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisasistlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisasistlabApplication.class, args);
+	}
+}
---
+++ src/test/java/pe/edu/sistemas/sisasistlab/SisasistlabApplicationTests.java
@@ -0,0 +1,16 @@
+package pe.edu.sistemas.sisasistlab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SisasistlabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
